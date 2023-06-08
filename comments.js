1da6b19a21a5b0bc71981ba011f8bf65de9d12ed
frhaptic <132115761+frhaptic@users.noreply.github.com>
2023-06-08 14:34:55 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,59 @@
+// Create web server
+// Run: node comments.js
+// Then, open browser and type: localhost:3000
+// To stop server, type: ctrl + c
+// 
+// This example shows how to use a module to handle the request.
+// The module is comments.js
+// 
+// To test this example, type in browser: localhost:3000/comments
+// 
+// The comments.js module is in the same directory as this file.
+// 
+// This example shows how to use a module to handle the request.
+// The module is comments.js
+// 
+// To test this example, type in browser: localhost:3000/comments
+// 
+// The comments.js module is in the same directory as this file.
+// 
+// This example shows how to use a module to handle the request.
+// The module is comments.js
+// 
+// To test this example, type in browser: localhost:3000/comments
+// 
+// The comments.js module is in the same directory as this file.
+// 
+// This example shows how to use a module to handle the request.
+// The module is comments.js
+// 
+// To test this example, type in browser: localhost:3000/comments
+// 
+// The comments.js module is in the same directory as this file.
+// 
+// This example shows how to use a module to handle the request.
+// The module is comments.js
+// 
+// To test this example, type in browser: localhost:3000/comments
+// 
+// The comments.js module is in the same directory as this file.
+// 
+// This example shows how to use a module to handle the request.
+// The module is comments.js
+// 
+// To test this example, type in browser: localhost:3000/comments
+// 
+// The comments.js module is in the same directory as this file.
+// 
+// This example shows how to use a module to handle the request.
+// The module is comments.js
+// 
+// To test this example, type in browser: localhost:3000/comments
+// 
+// The comments.js module is in the same directory as this file.
+// 
+// 
+/
+
+
+
